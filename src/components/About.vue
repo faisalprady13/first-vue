521ca59d87667c1afe48a0881d44684d77b6f708
faisalprady <faisal.prady@gravityandstorm.de>
2022-01-17 18:18:51 +0100

    base vue with routing example

---
+++ src/components/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>hallo its {{ testVal }}</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            testVal: 'ABOUT',
+        };
+    },
+};
+</script>
+
+<style></style>
---
+++ src/components/Home.vue
@@ -0,0 +1,7 @@
+<template lang="">
+    <div>this is home</div>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
